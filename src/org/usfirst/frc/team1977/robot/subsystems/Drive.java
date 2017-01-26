85e7625239f8e67a7d582812aaee0576bcc6ba34
haogg19 <haogg19@.thompsonschools.org>
2017-01-26 16:29:29 -0700

    Added code for setting motor controllers in drive subsystem

--- src/org/usfirst/frc/team1977/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team1977/robot/subsystems/Drive.java
@@ -18,11 +18,20 @@ public class Drive extends Subsystem {
 	 private Victor backRight;
 	
 	public Drive(){
+		//Define motor controllers for main drive train
 		frontLeft = new Victor(RobotMap.DRIVE_FRONT_LEFT_TALON);
     	frontRight = new Victor(RobotMap.DRIVE_FRONT_RIGHT_TALON);
     	backLeft = new Victor(RobotMap.DRIVE_BACK_LEFT_TALON);
     	backRight = new Victor(RobotMap.DRIVE_BACK_RIGHT_TALON);
 	}
+	
+	//Code for controlling the motor controllers last year
+	/*public void drive(double hPower, double vPower, double turn) {
+        frontLeft.set((vPower - hPower + (turn * turnPowerCoefficient)) * voltageCoefficient);
+        frontRight.set((-vPower - hPower + (turn * turnPowerCoefficient)) * voltageCoefficient);
+        backLeft.set((vPower + hPower + (turn * turnPowerCoefficient)) * voltageCoefficient);
+        backRight.set((-vPower + hPower + (turn * turnPowerCoefficient)) * voltageCoefficient);
+    }*/
 	
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
