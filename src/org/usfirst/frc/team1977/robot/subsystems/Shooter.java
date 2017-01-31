3f9b0c617fafcfdb9a106ea9e875ebc48670b4a9
ckworrell17 <ckworrell17@LO0145W01050255.thompsonschools.org>
2017-01-31 16:48:03 -0700

    Shooter Enters

--- src/org/usfirst/frc/team1977/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team1977/robot/subsystems/Shooter.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team1977.robot.subsystems;
 
+import org.usfirst.frc.team1977.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -8,9 +11,29 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Shooter extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
-
+	private static Shooter instance;
+	
+	//private Victor Shoot1;
+	private Victor Shoot1;
+	
+	public Shooter(){
+		Shoot1 = new Victor(RobotMap.SHOOT1MOTOR);
+	}
+	
 	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
-		// setDefaultCommand(new MySpecialCommand());
+    }
+	
+	public void ShootPower(double Power) {
+		Shoot1.set(Power);
+	}
+	
+	public void stop() {
+    	ShootPower(0);
+    }
+
+	public static Shooter getInstance() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
+
